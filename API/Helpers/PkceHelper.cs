8d3ca05ac849d695794cdd7380775762914d70db
Karim ElSedfy <kariemelsedfy@gmail.com>
2025-04-21 16:23:54 -0400

    added helper for code challenge and also started Oauth process

---
+++ API/Helpers/PkceHelper.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class PkceHelper
+    {
+        public static string GenerateCodeChallegne() {
+            byte[] bytes = new byte[96];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(bytes);
+            return Base64UrlEncode(bytes);
+        }
+
+        private static string Base64UrlEncode(byte[] input) {
+            return Convert.ToBase64String(input)
+                .TrimEnd('=')
+                .Replace('+', '-')
+                .Replace('/', '_');
+        }
+
+
+    }
+}
